76490752fd806c0b4d47660f8cae81581dff86c2
Cliffest <cliffest17@gmail.com>
2025-03-04 14:46:37 +0800

    2nd commit

---
+++ train_model.py
@@ -0,0 +1,11 @@
+# train_model.py
+import sys
+import os
+
+# 添加当前目录到路径，确保可以导入language_model包
+sys.path.insert(0, os.path.abspath('.'))
+
+from gpt.train import main
+
+if __name__ == "__main__":
+    main()
